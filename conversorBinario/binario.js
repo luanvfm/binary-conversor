39e7659b4a8f77979caa88046f731812ecfc8bbd
luanvfm <luanventurafm2@gmail.com>
2025-08-17 15:00:08 -0300

    codigo refatorado

--- conversorBinario/binario.js
+++ conversorBinario/binario.js
@@ -1,24 +1,31 @@
 let numero = parseFloat(prompt("Insira o seu numero a ser transformado em binario: "))
 let binario = 0 
-let listaBinario = []
 
-if (numero == 0) {
-    listaBinario.push(numero)
-}
-else {
-
-    while (numero > 0){
-
-    binario = numero % 2 //Pega o resto de numero 
-    listaBinario.push(binario) //Append pra lista de binarios
-    numero = Math.floor(numero / 2) //Arrendonda pra baixo    
-     
+function converterParaBinario(numero) {
+    
+    let listaBinario = []
+    
+    if (numero == 0) {
+        listaBinario.push(numero)
     }
+    else {
+        
+        while (numero > 0){
+            
+            binario = numero % 2 //Pega o resto de numero 
+            listaBinario.push(binario) //Append pra lista de binarios
+            numero = Math.floor(numero / 2) //Arrendonda pra baixo    
+            
+        }
+    }
+    
+    
+    listaBinario.reverse()
+    binarioFinal = listaBinario.join('')
+    
+    return binarioFinal
 }
 
-
-listaBinario.reverse()
-binarioFinal = listaBinario.join('')
-
+converterParaBinario(numero)
 
 alert(`Seu numero convertido é ${binarioFinal}`)
